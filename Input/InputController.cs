6203dd509133d2fb27e231953d522e6bd3c95bac
Edward Creighton <eduard.chekukha@mail.ru>
2024-01-04 13:12:34 +0300

    Added LevelController.cs

--- Input/InputController.cs
+++ Input/InputController.cs
@@ -1,9 +1,20 @@
 ﻿using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace Sterling.Input
 {
 	public class InputController : MonoBehaviour
 	{
-		
+		[SerializeField] private PlayerInput playerInput;
+
+		private SterlingInputActions _sterlingInputActions;
+
+		private void Awake()
+		{
+			_sterlingInputActions = new SterlingInputActions();
+			playerInput.actions = _sterlingInputActions.asset;
+			
+			_sterlingInputActions.Player.Enable();
+		}
 	}
 }
---
+++ Level/LevelController.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using Sterling.Input;
+
+namespace Sterling
+{
+	public class LevelController : MonoBehaviour
+	{
+		[SerializeField] private InputController inputController;
+
+		public InputController InputController => inputController;
+	}
+}
